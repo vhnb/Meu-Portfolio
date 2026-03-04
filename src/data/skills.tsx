eccf8cfbf60171b8ff821d72685cbd6d47a7e338
Victor Henrique. <123006884+vhnb@users.noreply.github.com>
2026-03-04 17:28:22 -0300

    Update skills.tsx

--- src/data/skills.tsx
+++ src/data/skills.tsx
@@ -1,4 +1,4 @@
-import { TbBrandJavascript, TbBrandTypescript, TbBrandNextjs, TbBrandReact, TbBrandHtml5, TbBrandCss3, TbBrandTailwind, TbBrandSvelte } from "react-icons/tb"
+import { TbBrandJavascript, TbBrandTypescript, TbBrandNextjs, TbBrandReact, TbBrandHtml5, TbBrandCss3, TbBrandTailwind, TbBrandSvelte, TbBrandGit } from "react-icons/tb"
 import { ReactNode } from "react"
 
 interface Skill {
@@ -20,5 +20,6 @@ export const skills: Skill[] = [
     { id: 6, icon: <TbBrandHtml5 {...iconStyle} />, alt: "HTML" },
     { id: 7, icon: <TbBrandCss3 {...iconStyle} />, alt: "CSS" },
     { id: 11, icon: <TbBrandTailwind {...iconStyle} />, alt: "Tailwind" },
-    { id: 13, icon: <TbBrandSvelte {...iconStyle} />, alt: "Svelte", }
+    { id: 13, icon: <TbBrandSvelte {...iconStyle} />, alt: "Svelte", },
+    { id: 13, icon: <TbBrandGit {...iconStyle} />, alt: "Git", }
 ]
